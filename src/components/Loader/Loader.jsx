01b5a58ace82db01ca64c88a8c2e29faa15029fc
Влад Язиков <vladzaraza@gmail.com>
2023-06-10 21:57:37 +0300

    123

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Audio, InfinitySpin } from 'react-loader-spinner';
+import { InfinitySpin } from 'react-loader-spinner';
 
 const StyledAudioWrapper = styled.div`
   position: fixed;
@@ -19,14 +19,6 @@ const StyledAudioWrapper = styled.div`
 export const Loader = () => {
   return (
     <StyledAudioWrapper className="audiowrapper">
-      {/* <Audio
-        height="80"
-        width="80"
-        radius="9"
-        color="green"
-        ariaLabel="three-dots-loading"
-        wrapperClass="audiowrapper"
-      /> */}
       <InfinitySpin width="200" color="#4fa94d" />
     </StyledAudioWrapper>
   );
